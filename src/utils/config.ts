d6ffddbc5d6c49f9ca765f07cd776422bdb0b3c9
p1atdev <shu_teiei@outlook.jp>
2022-03-29 06:32:22 +0900

    fix: config.json loading logic

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@
 // import { resolvePath } from "@nuxt/kit"
 
 export const supportedSites = async () => {
-    const url = process.server ? "http://localhost:3000" : window.location.origin
+    const url = process.env.VERCEL_ENV ? "http://localhost:3000" : process.env.VERCEL_URL ?? window.location.origin
 
     // const sites = fs.readFileSync(await resolvePath(`public/assets/sites.json`), "utf-8")
     // const
