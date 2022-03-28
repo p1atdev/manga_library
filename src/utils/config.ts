19cc703399ea673dc5d871324b3353e595fd28f4
p1atdev <shu_teiei@outlook.jp>
2022-03-29 06:42:30 +0900

    fix: vercel url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import config from "#config"
 
 export const supportedSites = async () => {
-    const url = config.VERCEL_ENV ? config.VERCEL_URL ?? window.location.origin : "http://localhost:3000"
+    const url = config.VERCEL_ENV ? "https://" + config.VERCEL_URL ?? window.location.origin : "http://localhost:3000"
 
     const json = await fetch(url + "/assets/sites.json").then((res) => res.json())
 
